48b0f2cd0a7720eaea92933b69a6d90111ed31e4
Stefano <stefano.pola.fargas@gmail.com>
2021-01-10 17:57:29 +0100

    Afegir els @ComponentScan de tots els packages

--- src/main/java/com/application/main/MainApp.java
+++ src/main/java/com/application/main/MainApp.java
@@ -2,8 +2,13 @@ package com.application.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.application.controller") 
+@ComponentScan(basePackages = "com.application.service") 
+@ComponentScan(basePackages = "com.application.repository")
+@ComponentScan(basePackages = "com.application.domain") 
 public class MainApp {
 
 	public static void main(String[] args) {
