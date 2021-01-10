5f4e89ae40f50a06a1b4297ba75b4b9f3945b9e1
Stefano <stefano.pola.fargas@gmail.com>
2021-01-10 18:14:05 +0100

    Creacio de la classe RestController

---
+++ src/main/java/com/application/controller/ShopController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author stefano
+ *
+ */
+
+@RestController
+public class ShopController {
+
+}
