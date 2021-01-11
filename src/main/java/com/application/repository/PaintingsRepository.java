58bdcd475f6e147388d1cff80156d4da114d9b96
Stefano <stefano.pola.fargas@gmail.com>
2021-01-11 02:35:06 +0100

    Addició de l'anotacio @Repository

--- src/main/java/com/application/repository/PaintingsRepository.java
+++ src/main/java/com/application/repository/PaintingsRepository.java
@@ -4,12 +4,14 @@
 package com.application.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import com.application.domain.*;
 
 /**
  * @author stefano
  *
  */
+@Repository
 public interface PaintingsRepository extends CrudRepository<Painting,Long> {
 	
 	
--- src/main/java/com/application/repository/ShopsRepository.java
+++ src/main/java/com/application/repository/ShopsRepository.java
@@ -4,14 +4,18 @@
 package com.application.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.application.domain.*;
 
 /**
  * @author stefano
  *
  */
+
+@Repository
 public interface ShopsRepository extends CrudRepository<Shop,Long>  {
 
 	Shop findByName (String name);
-	
+		
 }
