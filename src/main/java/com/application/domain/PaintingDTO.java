244f8f3b37b6f3fcbabc425c8eaa702b31d8eccd
Stefano <stefano.pola.fargas@gmail.com>
2021-01-11 01:40:48 +0100

    Eliminació de diferents imports que no servien

--- src/main/java/com/application/domain/PaintingDTO.java
+++ src/main/java/com/application/domain/PaintingDTO.java
@@ -3,17 +3,6 @@
  */
 package com.application.domain;
 
-import java.util.Calendar;
-
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  * @author stefano
