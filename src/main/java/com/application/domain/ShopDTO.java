7fe0171ca6a1ed4a0cb6d332649923d50227b8a5
Stefano <stefano.pola.fargas@gmail.com>
2021-01-11 01:35:38 +0100

    Implementacio de la classe ShopDTO

--- src/main/java/com/application/domain/ShopDTO.java
+++ src/main/java/com/application/domain/ShopDTO.java
@@ -3,10 +3,54 @@
  */
 package com.application.domain;
 
+
+
 /**
  * @author stefano
  *
  */
 public class ShopDTO {
+
+	private String name;
+	private int capacity;
+
+	public ShopDTO() {
+
+	}
+
+	public ShopDTO(String name, int capacity) {
+
+		this.name = name;
+		this.capacity = capacity;
+
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+
+	/**
+	 * @param capacity the capacity to set
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
 
 }
